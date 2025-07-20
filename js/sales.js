f42802156d18128e95e02c6b80e735adfca3849c
Datkep92 <nguyenquocdat1511992@gmail.com>
2025-07-20 23:21:40 +0700

    Update sales.js

--- js/sales.js
+++ js/sales.js
@@ -1,9 +1,7 @@
 import { database } from './firebase-config.js';
 import { ref, set, get, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { showError, showSuccess, parseNumber, capitalizeFirstLetter } from './utils.js';
-// sales.js (phần đầu file)
-import productModule from './products.js';
-const { products, renderProductSelection, loadProducts } = productModule;
+import { products, renderProductSelection, loadProducts } from './products.js';
 
 let dailyData = {
   date: '',
@@ -186,6 +184,7 @@ export function renderDailyData() {
   document.getElementById('dailyBalance').textContent = `${balance.toLocaleString('vi-VN')}₫`;
 }
 
+// Expose functions to window for HTML onclick handlers
 window.switchTab = switchTab;
 window.addExport = addExport;
 window.deleteExport = deleteExport;
