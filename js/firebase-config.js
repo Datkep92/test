0e40b7512bc14902891367762766bc6a4bedffc7
Datkep92 <nguyenquocdat1511992@gmail.com>
2025-07-20 22:41:09 +0700

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDmFpKa8TpDjo3pQADaTubgVpDPOi-FPXk",
-  authDomain: "quanly-d7e54.firebaseapp.com",
-  projectId: "quanly-d7e54",
-  storageBucket: "quanly-d7e54.firebasestorage.app",
-  messagingSenderId: "482686011267",
-  appId: "1:482686011267:web:f2fe9d400fe618487a98b6",
-  measurementId: "G-G3H6MTCWCM"
-};
