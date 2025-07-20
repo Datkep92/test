9d1cab65e3759b774906e239d61459179407b27c
Datkep92 <nguyenquocdat1511992@gmail.com>
2025-07-20 21:03:26 +0700

    Create routes.js

---
+++ public/js/config/routes.js
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  LOGIN: '/views/login.html',
+  ADMIN: '/views/admin.html',
+  STAFF: '/views/staff.html',
+  
+  API: {
+    PRODUCTS: '/products',
+    USERS: '/users',
+    SALES: '/sales'
+  }
+};
+
+export const ROLE = {
+  ADMIN: 'admin',
+  STAFF: 'staff'
+};
