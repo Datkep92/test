7e77cb54c7bf290df78bb4aa8644a16d0f92ec2f
Datkep92 <nguyenquocdat1511992@gmail.com>
2025-07-20 22:41:27 +0700

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,21 @@
+// Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmFpKa8TpDjo3pQADaTubgVpDPOi-FPXk",
+  authDomain: "quanly-d7e54.firebaseapp.com",
+  databaseURL: "https://quanly-d7e54-default-rtdb.firebaseio.com",
+  projectId: "quanly-d7e54",
+  storageBucket: "quanly-d7e54.firebasestorage.app",
+  messagingSenderId: "482686011267",
+  appId: "1:482686011267:web:f2fe9d400fe618487a98b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { app, auth, database };
