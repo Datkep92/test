8152a371b1251ad58fe042a00526fb4f04840c0f
Datkep92 <nguyenquocdat1511992@gmail.com>
2025-07-21 10:59:09 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDmFpKa8TpDjo3pQADaTubgVpDPOi-FPXk",
   authDomain: "quanly-d7e54.firebaseapp.com",
   databaseURL: "https://quanly-d7e54-default-rtdb.firebaseio.com",
@@ -7,3 +7,7 @@ export const firebaseConfig = {
   messagingSenderId: "482686011267",
   appId: "1:482686011267:web:f2fe9d400fe618487a98b6"
 };
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
