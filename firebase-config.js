b1527a94714836dabfbc8c88d9449f14584ff957
Datkep92 <nguyenquocdat1511992@gmail.com>
2025-07-21 20:54:50 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
-var firebaseConfig = {
+// Khởi tạo Firebase
+const firebaseConfig = {
   apiKey: "AIzaSyDmFpKa8TpDjo3pQADaTubgVpDPOi-FPXk",
   authDomain: "quanly-d7e54.firebaseapp.com",
   databaseURL: "https://quanly-d7e54-default-rtdb.firebaseio.com",
@@ -7,7 +8,12 @@ var firebaseConfig = {
   messagingSenderId: "482686011267",
   appId: "1:482686011267:web:f2fe9d400fe618487a98b6"
 };
+// Khởi tạo Firebase
+const app = firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
-var auth = firebase.auth();
-var db = firebase.database();
+// Xuất các module cần thiết
+const auth = firebase.auth();
+const db = firebase.database();
+
+// Xuất để các file khác sử dụng
+export { auth, db };
